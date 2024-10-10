40e89a84b6c2d6fe094e74e26b52c0e882e802ba
shin <vika.furlet@gmail.com>
2024-10-10 11:33:11 +0300

    Update second.py

--- second.py
+++ second.py
@@ -1,10 +1,11 @@
+import random
 mas = []
 mas11 = []
 
-for i in range(5):
-    number = int(input("введіть масив: "))
+for i in range(20):
+    number = random.randint(0, 100)
     mas.append(number)
-
+print(mas)
 for number in mas:
     if number % 11 == 0:
         mas11.append(number)
